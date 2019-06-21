66808aaa3864e4e291538dbe0f49c3148b4dd4cb
niceyou <652910234@qq.com>
2019-06-21 22:56:42 +0800

    登录功能

--- src/App.js
+++ src/App.js
@@ -8,6 +8,30 @@ import {
 
 import './App.css';
 import Login from './component/login/login.js';
+import axios from "axios";
+axios.defaults.baseURL = "http://47.96.21.88:8086/";
+
+// Add a request interceptor
+// axios.interceptors.request.use(function (config) {
+//     // Do something before request is sent
+//     return config;
+//   }, function (error) {
+//     // Do something with request error
+//     return Promise.reject(error);
+//   });
+
+// 响应拦截器
+axios.interceptors.response.use(
+  function(response) {
+    // Do something with response data
+    // console.log(response)
+    return response.data;
+  },
+  function(error) {
+    // Do something with response error
+    return Promise.reject(error);
+  }
+);
 
 class App extends Component {
   render() {
--- src/component/login/login.js
+++ src/component/login/login.js
@@ -1,75 +1,76 @@
-import React from 'react';
-import axios from 'axios';
-import {withRouter} from "react-router-dom";
-
-// Add a request interceptor
-// axios.interceptors.request.use(function (config) {
-//     // Do something before request is sent
-//     return config;
-//   }, function (error) {
-//     // Do something with request error
-//     return Promise.reject(error);
-//   });
-
-// 响应拦截器
-axios.interceptors.response.use(function (response) {
-    // Do something with response data
-    // console.log(response)
-    return response.data
-  }, function (error) {
-    // Do something with response error
-    return Promise.reject(error);
-  });
+import React from "react";
+import axios from "axios";
+import { withRouter } from "react-router-dom";
+import { Form } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
+import "./css/login.css";
 
 class Login extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      username: '',
-      pw: ''
-    }
-  }
-
-  handleUsername = (event) => {
-    this.setState({
-      username: event.target.value
-    });
-  }
-
-  handlePw = (event) => {
-    this.setState({
-      pw: event.target.value
-    });
+      uname: "",
+      pwd: ""
+    };
   }
 
   handleSubmit = () => {
-    const {history} = this.props;
-    // console.log(this.state.username,this.state.pw)
-    axios.post('http://47.96.21.88:8086/users/login',{
-      uname: this.state.username,
-      pwd: this.state.pw
-    }).then(result=>{
-      // console.log(data.data)
-      if(result.meta.status === 200) {
-        // 存储token
-        localStorage.setItem('mytoken', result.data.token);
-        // 登录成功
-        history.push('/home');
-      }
+    const { history } = this.props;
+    axios
+      .post("users/login", {
+        uname: this.state.uname,
+        pwd: this.state.pwd
+      })
+      .then(result => {
+        // console.log(data.data)
+        if (result.meta.status === 200) {
+          // 存储token
+          localStorage.setItem("mytoken", result.data.token);
+          // 登录成功
+          history.push("/home");
+        }
+      });
+  };
+
+  handleChange = event => {
+    // 从原生DOM中获取属性：name 和 value
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
     });
-  }
+  };
 
   render() {
+    const { uname, pwd } = this.state;
     return (
-      <div>
-        <div>登录</div>
-        <div>
-          用户名：<input value={this.state.username} onChange={this.handleUsername} type="text" name="username"/>
+      <div className="login-container">
+        <div className="login-title">登录</div>
+        <div className="login-form">
+          <Form onSubmit={this.handleSubmit}>
+            <Form.Input
+              icon="user"
+              required
+              size="big"
+              iconPosition="left"
+              name="uname"
+              value={uname}
+              onChange={this.handleChange}
+              placeholder="请输入用户名..."
+            />
+            <Form.Input
+              type="password"
+              icon="lock"
+              required
+              size="big"
+              iconPosition="left"
+              name="pwd"
+              value={pwd}
+              onChange={this.handleChange}
+              placeholder="请输入密码..."
+            />
+            <Form.Button positive content="登录" />
+          </Form>
         </div>
-        <div>
-          密码：<input value={this.state.pw} onChange={this.handlePw} type="password" name="pw"/>
-        </div>
-        <button onClick={this.handleSubmit}>登录</button>
       </div>
     );
   }
